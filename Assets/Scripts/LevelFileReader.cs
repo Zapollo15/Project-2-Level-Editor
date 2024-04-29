c2a531c3f06a1148d8c5b8b730b452d6cd870cda
Andrew Roberts <a.roberts@student.saedubai.com>
2024-04-29 13:03:40 +0400

    Save to file, tile editor

--- Assets/Scripts/LevelFileReader.cs
+++ Assets/Scripts/LevelFileReader.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using System.Linq;
 using UnityEngine;
 
 public class LevelFileReader : MonoBehaviour
@@ -22,7 +21,49 @@ public class LevelFileReader : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.F))
+        {
+            InstantiatePrefabOnHover(floorPrefab);
+        }
+        else if (Input.GetKey(KeyCode.P))
+        {
+            InstantiatePrefabOnHover(playerPrefab);
+        }
+        else if (Input.GetKey(KeyCode.B))
+        {
+            InstantiatePrefabOnHover(boxPrefab);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            InstantiatePrefabOnHover(wallPrefab);
+        }
+        else if (Input.GetKey(KeyCode.G))
+        {
+            InstantiatePrefabOnHover(goalPrefab);
+        }
+    }
 
+    void InstantiatePrefabOnHover(GameObject prefab)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            int x = Mathf.FloorToInt(hit.point.x / tileSize);
+            int y = Mathf.FloorToInt(hit.point.y / tileSize);
+
+            if (x >= 0 && x < grid.GetLength(1) && y >= 0 && y < grid.GetLength(0))
+            {
+                if (grid[y, x] != null)
+                {
+                    Destroy(grid[y, x]);
+                }
+
+                Vector3 position = new Vector3(x * tileSize, y * tileSize, 0);
+                grid[y, x] = Instantiate(prefab, position, Quaternion.identity);
+            }
+        }
     }
 
     void LoadLevelFromFile()
@@ -76,7 +117,7 @@ public class LevelFileReader : MonoBehaviour
                     switch (tile)
                     {
                         case '@':
-                            grid[i,j] = Instantiate(wallPrefab, position, Quaternion.identity);
+                            grid[i, j] = Instantiate(wallPrefab, position, Quaternion.identity);
                             break;
                         case ' ':
                             grid[i, j] = Instantiate(floorPrefab, position, Quaternion.identity);
@@ -97,5 +138,49 @@ public class LevelFileReader : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void SaveLevelToFile()
+    {
+        int rows = grid.GetLength(0);
+        int cols = grid.GetLength(1);
+
+        string[] levelData = new string[rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            string row = "";
+            for (int j = 0; j < cols; j++)
+            {
+                if (grid[i, j] == null)
+                {
+                    row += " ";
+                }
+                else if (grid[i, j].CompareTag("Wall"))
+                {
+                    row += "@";
+                }
+                else if (grid[i, j].CompareTag("Floor"))
+                {
+                    row += " ";
+                }
+                else if (grid[i, j].CompareTag("Goal"))
+                {
+                    row += "x";
+                }
+                else if (grid[i, j].CompareTag("Box"))
+                {
+                    row += "o";
+                }
+                else if (grid[i, j].CompareTag("Player"))
+                {
+                    row += "<";
+                }
+            }
+            levelData[i] = row;
+        }
+
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+        File.WriteAllLines(filePath, levelData);
     }
 }
